ba7580ecba45885a108aecf0f0d3b0287d981f6e
Daniel Poda <daniel.poda@gmail.com>
2019-04-26 21:36:54 -0600

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,142 @@
+<script>
+  import SvelteTable from "./SvelteTable.svelte";
+  import faker from "faker";
+  faker.seed(5);
+
+  console.log(faker);
+  export let name;
+
+  const colums = [
+    {
+      key: "id",
+      title: "ID",
+      value: v => v.id,
+      sortable: true,
+      filterOptions: rows => {
+        let nums = {};
+        rows.forEach(row => {
+          let num = Math.floor(row.id / 10);
+          if (nums[num] === undefined)
+            nums[num] = { name: `${num * 10} to ${(num + 1) * 10}`, value: num };
+        });
+        // fix order
+        nums = Object.entries(nums)
+          .sort()
+          .reduce((o, [k, v]) => ((o[k] = v), o), {});
+        return Object.values(nums);
+      },
+      filterValue: v => Math.floor(v.id / 10),
+      headerClass: 'text-left'
+    },
+    {
+      key: "first_name",
+      title: "FIRST_NAME",
+      value: v => v.first_name,
+      sortable: true,
+      filterOptions: rows => {
+        let letrs = {};
+        rows.forEach(row => {
+          let letr = row.first_name.charAt(0);
+          if (letrs[letr] === undefined)
+            letrs[letr] = {
+              name: `${letr.toUpperCase()}`,
+              value: letr.toLowerCase()
+            };
+        });
+        // fix order
+        letrs = Object.entries(letrs)
+          .sort()
+          .reduce((o, [k, v]) => ((o[k] = v), o), {});
+        return Object.values(letrs);
+      },
+      filterValue: v => v.first_name.charAt(0).toLowerCase()
+    },
+    {
+      key: "last_name",
+      title: "LAST_NAME",
+      value: v => v.last_name,
+      sortable: true,
+      filterOptions: rows => {
+        let letrs = {};
+        rows.forEach(row => {
+          let letr = row.last_name.charAt(0);
+          if (letrs[letr] === undefined)
+            letrs[letr] = {
+              name: `${letr.toUpperCase()}`,
+              value: letr.toLowerCase()
+            };
+        });
+        // fix order
+        letrs = Object.entries(letrs)
+          .sort()
+          .reduce((o, [k, v]) => ((o[k] = v), o), {});
+        return Object.values(letrs);
+      },
+      filterValue: v => v.last_name.charAt(0).toLowerCase()
+    },
+    {
+      key: "email",
+      title: "EMAIL",
+      value: v => v.email,
+      sortable: true,
+      class: 'text-center'
+    },
+    {
+      key: "gender",
+      title: "GENDER",
+      value: v => v.gender,
+      renderValue: v => {
+        const classNames = [`g_${v.gender.toLowerCase()}`];
+        let icon = v.gender.toLowerCase() === "female" ? "⚩" : "";
+        icon = v.gender.toLowerCase() === "male" ? "⚨" : icon;
+        return `<span class="${classNames.join(" ")}">${v.gender} ${icon}</span>`;
+      },
+      sortable: true,
+      filterOptions: [
+        { name: "Male", value: "Male" },
+        { name: "Female", value: "Female" }
+      ],
+      filterValue: v => v.gender
+    },
+    {
+      key: "ip_address",
+      title: "IP_ADDRESS",
+      value: v => v.ip_address,
+      sortable: true
+    }
+  ];
+
+  const numRows = 50;
+  const data = Array(numRows)
+    .fill('')
+    .map((n, i) => {
+      let d = {
+        id: i,
+        first_name: faker.name.firstName(),
+        last_name: faker.name.lastName(),
+        gender: faker.random.number(1) ? "Female" : "Male",
+        ip_address:
+          "192.168." + faker.random.number(128) + "." + faker.random.number(255)
+      };
+      d.email = d.first_name[0].toLowerCase() + d.last_name.toLowerCase() + '@zipit.org.ca';
+      return d;
+    });
+</script>
+<style>
+	div :global(.g_female) {
+	  color: #f9e;
+	}
+	div :global(.g_male) {
+	  color: #99e;
+  }
+  div :global(.text-center) {
+    text-align: center;
+  }
+  div :global(.text-left) {
+    text-align: left;
+  }
+</style>
+<div><span class="g_female"></span></div>
+<div>
+	<SvelteTable columns={colums} rows={data}></SvelteTable>
+</div>
---
+++ src/SvelteTable.svelte
@@ -0,0 +1,110 @@
+<script>
+	export let columns = [];
+	export let rows = [];
+
+	let sortOrder = 1;
+	let sortKey = "";
+	let sortBy = r => "";
+	let showFilterHeader = columns.some(c => c.filterOptions !== undefined);
+	let filterValues = {};
+	let filterSettings = {};
+	let columnByKey = {};
+	columns.forEach(col => {
+	  columnByKey[col.key] = col;
+	});
+
+	// $: console.log(filterSettings, filterValues);
+	$: c_rows = rows
+	  .filter(r =>
+	    Object.keys(filterSettings).every(f => {
+	      // console.log(f, filterSettings[f], columnByKey[f])
+	      let ret =  (
+	        filterSettings[f] === undefined ||
+	        filterSettings[f] === columnByKey[f].filterValue(r)
+				);
+				return ret;
+	    })
+	  )
+	  .map(r => ({ ...r, $sortOn: sortBy(r) }))
+	  .sort((a, b) => {
+	    if (a.$sortOn > b.$sortOn) return sortOrder;
+	    else if (a.$sortOn < b.$sortOn) return -sortOrder;
+	    return 0;
+	  });
+
+	const calculateFilterValues = () => {
+	  filterValues = {};
+	  columns.forEach(c => {
+	    if (typeof c.filterOptions === "function") {
+	      filterValues[c.key] = c.filterOptions(rows);
+	    } else if (Array.isArray(c.filterOptions)) {
+	      filterValues[c.key] = [...c.filterOptions];
+	    }
+	  });
+	};
+
+	const handleSort = col => {
+	  if (col.sortable === true && typeof col.value === "function") {
+	    if (sortKey === col.key) {
+	      sortOrder = sortOrder === 1 ? -1 : 1;
+	    } else {
+	      sortOrder = 1;
+	      sortKey = col.key;
+	      sortBy = r => col.value(r);
+	    }
+	  }
+	};
+
+	if (showFilterHeader) {
+	  calculateFilterValues();
+	}
+</script>
+
+<style>
+	table {
+	  width: 100%;
+	}
+	.isSortable {
+	  cursor: pointer;
+	}
+
+	tr th select {
+		width: 100%;
+	}
+</style>
+
+<table>
+	{#if showFilterHeader}
+		<tr>
+		  {#each columns as col}
+		  	<th>
+					{#if filterValues[col.key] !== undefined}
+						<select bind:value={filterSettings[col.key]}>
+							<option value={undefined}></option>
+						  {#each filterValues[col.key] as option}
+								<option value={option.value}>{option.name}</option>
+						  {/each}
+						</select>
+					{/if}
+				</th>
+		  {/each}
+		</tr>
+	{/if}
+	<tr>
+	  {#each columns as col}
+	  	<th on:click={() => handleSort(col)} class="{[(col.sortable ? 'isSortable' : '' ),(col.headerClass !== undefined && col.headerClass)].join(' ')}">
+				{col.title}
+				{#if sortKey === col.key}
+					{ sortOrder === 1 ? '▲' : '▼'}
+				{/if}
+			</th>
+	  {/each}
+	</tr>
+	{#each c_rows as row}
+		<tr>
+		  {#each columns as col}
+		  	<td class="{col.class !== undefined && col.class}">{@html col.renderValue ? col.renderValue(row) : col.value(row)}</td>
+		  {/each}
+		</tr>
+	{/each}
+</table>
