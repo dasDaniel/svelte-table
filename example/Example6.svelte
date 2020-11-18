9e2d9ec5a0d5c50603d1385dfebb3217e6c8fc70
thilo-behnke <thilo.behnke@gmx.net>
2020-11-18 05:08:32 +0100

    Feature / insert component into cell (#21)
    * feature/render-component-in-cell
    
    * cleanup/remove-unused-css-in-example

---
+++ example/Example6.svelte
@@ -0,0 +1,74 @@
+<script>
+  import SvelteTable from "../src/SvelteTable.svelte";
+  import ContactButtonComponent from "./example6/ContactButtonComponent.svelte";
+  // import SvelteTable from "svelte-table";
+  import faker from "faker";
+  faker.seed(15);
+
+  const colums = [
+    {
+      key: "id",
+      title: "ID",
+      value: v => v.id,
+      sortable: false,
+      headerClass: "text-left"
+    },
+    {
+      key: "first_name",
+      title: "FIRST NAME",
+      value: v => v.first_name,
+      sortable: true,
+      searchValue: v => v.first_name
+    },
+    {
+      key: "last_name",
+      title: "LAST NAME",
+      value: v => v.last_name,
+      sortable: true,
+      searchValue: v => v.last_name
+    },
+    {
+      key: "email",
+      title: "EMAIL",
+      value: v => v.email,
+      sortable: true,
+      class: "text-center"
+    },
+    {
+      key: "actions",
+      title: "",
+      sortable: false,
+      renderComponent: ContactButtonComponent
+    },
+  ];
+
+  const numRows = 50;
+  const data = Array(numRows)
+    .fill("")
+    .map((n, i) => {
+      let d = {
+        id: i,
+        first_name: faker.name.firstName(),
+        last_name: faker.name.lastName(),
+        gender: faker.random.number(1) ? "Female" : "Male",
+        ip_address:
+          "192.168." + faker.random.number(128) + "." + faker.random.number(255)
+      };
+      d.email =
+        d.first_name[0].toLowerCase() +
+        d.last_name.toLowerCase() +
+        "@zipit.org.ca";
+      return d;
+    });
+</script>
+
+<style>
+  div :global(.text-center) {
+    text-align: center;
+  }
+  div :global(.text-left) {
+    text-align: left;
+  }
+</style>
+
+<SvelteTable columns={colums} rows={data} />
